984189b07ea0b28d7648700d94ab7af694dec16e
Tomas <tomascerdeyraimport@gmail.com>
2023-10-23 15:07:18 -0300

    carpetas

--- src/main/java/com/mycompany/gestordeturnos/GestorDeTurnos.java
+++ src/main/java/com/mycompany/gestordeturnos/GestorDeTurnos.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.gestordeturnos;
 
-/**
- *
- * @author Usuario
- */
 public class GestorDeTurnos {
 
     public static void main(String[] args) {
