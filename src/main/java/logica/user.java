b586f0b6fade64285f2d95deab4f834e7ce2eef3
Tomas <tomascerdeyraimport@gmail.com>
2023-10-24 10:21:17 -0300

    arreglo

---
+++ src/main/java/logica/user.java
@@ -0,0 +1,19 @@
+package logica;
+
+public class user {
+    String name = "user";
+    String password = "405314";
+    public user() {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }   
+   
+}
