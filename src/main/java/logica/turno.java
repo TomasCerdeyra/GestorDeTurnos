877b56197e877d4991cdb57d1f7070f2a3dc9985
Juan Cruz Dauberte <juancruzdauberteeeee@gmail.com>
2023-10-25 14:30:59 -0300

    Logica turnos y obra social

---
+++ src/main/java/logica/turno.java
@@ -0,0 +1,20 @@
+
+package logica;
+
+/**
+ *
+ * @author Juan Cruz
+ */
+import java.util.ArrayList;
+import java.util.List;
+import logica.paciente;
+
+public class turno {
+     public static List<Turnos> listaTurnos = new ArrayList<>();
+     
+     public class Turnos{
+         private String hora;
+         private String 
+    
+}
+}
