bc8c86eee271a86c1995e2a2168efd3db6d04735
matilopezz <matilopezo25@gmail.com>
2023-10-25 15:30:56 -0300

    Cambios 2

--- src/main/java/logica/obrasocial.java
+++ src/main/java/logica/obrasocial.java
@@ -1,4 +1,3 @@
-
 package logica;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,15 +5,28 @@ import logica.paciente.Pacientes;
 
 public class obrasocial {
     public static List<ObraSocial> listaObraSocial = new ArrayList<>();
-    
-    public  class ObraSocial{
-     paciente obraSocial = new paciente();
-     
-      
-             
-             
-             
-             
-             
+
+    static {
+        listaObraSocial.add(new ObraSocial("A"));
+        listaObraSocial.add(new ObraSocial("B"));
+        listaObraSocial.add(new ObraSocial("C"));
+        listaObraSocial.add(new ObraSocial("D"));
+        listaObraSocial.add(new ObraSocial("E"));
+    }
+
+    public static class ObraSocial {
+        private String nombre;
+        Pacientes obraSocial = new Pacientes();
+
+
+        public ObraSocial(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
     }
 }
+
+
